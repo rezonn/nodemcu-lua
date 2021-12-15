2f7ee0076c583cd165d8dce092e6962b99102d15
rezonn <rezonn@msn.com>
2021-12-16 01:00:31 +0500

    Add files via upload

---
+++ win10.bat
@@ -0,0 +1,15 @@
+esptool --port com3 erase_flash
+esptool --port com3 write_flash --flash_size=detect 0 nodemcu.bin
+
+nodemcu-uploader --port com3 file list
+
+nodemcu-uploader upload init.lua
+
+
+nodemcu-uploader file print init.lua
+nodemcu-uploader node restart
+nodemcu-uploader node heap
+nodemcu-uploader file remove foo.lua
+
+esptool --port com3 write_flash --flash_size=detect 0 integer.bin
+
